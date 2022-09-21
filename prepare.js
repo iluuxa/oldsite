4c4145c1c14c24ecea6d35e8508ec7d3a892e29d
Ilya <yasko2001@yandex.ru>
2022-09-21 19:26:09 +0300

    deployment 4, changing title

--- prepare.js
+++ prepare.js
@@ -1,7 +1,15 @@
 $(function () {
-    $("#header-image").load("/header_image.html");
-    $("#header-text").load("/header_text.html");
-    $("#menu").load("/menu.html");
-    let container = document.getElementById("main-container");
-    container.style.height=window.innerHeight.toString();
-});
+        var title = document.getElementById("prepare-script").getAttribute("data-title");
+        console.log(typeof title);
+        if (typeof title != "string") {
+            title = "Стихотворение";
+        }
+        $("#header-image").load("/header_image.html");
+        $("#header-text").load("/header_text.html", function () {
+            document.getElementById("upper-text").textContent = title;
+        });
+        $("#menu").load("/menu.html");
+        let container = document.getElementById("main-container");
+        container.style.height = window.innerHeight.toString();
+    }
+);
